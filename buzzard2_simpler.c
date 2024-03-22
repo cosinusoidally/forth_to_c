d56f1b1fa889de8bde643bfedbeade9c08c487c3
Liam Wilson <cosinusoidally@gmail.com>
2024-03-22 20:27:28 +0000

    more

--- buzzard2_simpler.c
+++ buzzard2_simpler.c
@@ -42,6 +42,30 @@ int     stack_ptr = 0;
 // m[0]: dictionary pointer
 // m[1]: return stack index
 
+wi8(o, v){
+  *(char*)o = (v & 255);
+}
+
+ri8(o){
+ return *(char*)o;
+}
+
+wi32(o, v) {
+  wi8(o,v&0xFF);
+  v=v>>8;
+  wi8(o+1,v&0xFF);
+  v=v>>8;
+  wi8(o+2,v&0xFF);
+  v=v>>8;
+  wi8(o+3,v&0xFF);
+}
+
+ri32(o) {
+  return (ri8(o)&255)       | (ri8(o+1)&255)<<8 |
+         (ri8(o+2)&255)<<16 | (ri8(o+3)&255)<<24;
+}
+
+
 append_to_dict(val) {
     int addr = m[0];
     m[addr] = val;
